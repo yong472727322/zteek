260da66f90854ad6be7f061319f356c5f81b2421
DESKTOP-7QQNVE1\zhs-pc <zhiyong@zttek.cn>
2018-10-21 16:15:12 +0800

    日志

--- src/main/java/com/zteek/controller/AmazonController.java
+++ src/main/java/com/zteek/controller/AmazonController.java
@@ -216,8 +216,10 @@ public class AmazonController {
      */
     @RequestMapping("recordLog")
     public void recordLog(PhoneLog log){
-        logger.info("记录手机[{}]发送过来的日志[{}]",log.getImei(),log.getMessage());
-        phoneService.recordLog(log);
+        if(!StringUtils.isEmpty(log.getImei()) && !StringUtils.isEmpty(log.getMessage())){
+            logger.info("记录手机[{}]发送过来的日志[{}]",log.getImei(),log.getMessage());
+            phoneService.recordLog(log);
+        }
     }
 
     /**
