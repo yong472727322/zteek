04e6c67114f940820d4399e6ba5de86d950dad57
yong472727322 <472727322@qq.com>
2018-10-31 16:06:40 +0800

    防止死锁

--- src/main/java/com/zteek/service/impl/IpPoolServiceImpl.java
+++ src/main/java/com/zteek/service/impl/IpPoolServiceImpl.java
@@ -71,107 +71,107 @@ public class IpPoolServiceImpl implements IpPoolService {
 
     @Override
     public synchronized IpPool getNewIpByImei(String imei) {
-        synchronized (IpPoolServiceImpl.class){
-            if(flagGetIp){
-                log.warn(" 有手机正在获取IP，返回null。");
-                return null;
+        try{
+            synchronized (IpPoolServiceImpl.class){
+                if(flagGetIp){
+                    log.warn("有手机正在获取IP，返回null。");
+                    return null;
+                }
+                flagGetIp = true;
             }
-            flagGetIp = true;
-        }
+            //遍历 VPS ，找到未使用过的IP
+            String notUseIp = null;
+            for(Map.Entry<String,String> vps : Constant.vps.entrySet()){
+                //判断VPS状态
+                if(Constant.vps_state.get(vps.getKey())){
+                    log.info("VPS[{}]正在更换IP中，不可用。",vps.getKey());
+                    continue;
+                }
+                //想用的IP，判断是否存在使用记录
+                String ip = vps.getValue();
+                boolean flag = true;
+                if(null != Constant.use && !Constant.use.isEmpty()){
+                    Map<String, Date> useRecord = Constant.use.get(ip);
 
-        //遍历 VPS ，找到未使用过的IP
-        String notUseIp = null;
-        for(Map.Entry<String,String> vps : Constant.vps.entrySet()){
-            //判断VPS状态
-            if(Constant.vps_state.get(vps.getKey())){
-                log.info("VPS[{}]正在更换IP中，不可用。",vps.getKey());
-                continue;
-            }
-            //想用的IP，判断是否存在使用记录
-            String ip = vps.getValue();
-            boolean flag = true;
-            if(null != Constant.use && !Constant.use.isEmpty()){
-                Map<String, Date> useRecord = Constant.use.get(ip);
-
-                if(null != useRecord && !useRecord.isEmpty()){
-                    int size = useRecord.size();
-                    Integer vpsChange = Constant.vps_change.get(vps.getKey());
-                    //如果此IP已经超过使用次数，则换其它的
-                    if(size >= vpsChange){
-                        log.info("此IP[{}]使用次数[{}]，已经达到目标次数[{}]，换其它IP",ip, size, vpsChange);
-                        continue;
-                    }
-                    for(Map.Entry<String,Date> use : useRecord.entrySet()){
-                        String key = use.getKey();
-                        if(imei.equalsIgnoreCase(key)){
-                            flag = false;
+                    if(null != useRecord && !useRecord.isEmpty()){
+                        int size = useRecord.size();
+                        Integer vpsChange = Constant.vps_change.get(vps.getKey());
+                        //如果此IP已经超过使用次数，则换其它的
+                        if(size >= vpsChange){
+                            log.info("此IP[{}]使用次数[{}]，已经达到目标次数[{}]，换其它IP",ip, size, vpsChange);
+                            continue;
+                        }
+                        for(Map.Entry<String,Date> use : useRecord.entrySet()){
+                            String key = use.getKey();
+                            if(imei.equalsIgnoreCase(key)){
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if(flag){
+                            notUseIp = ip;
                             break;
                         }
-                    }
-                    if(flag){
+                    }else {
+                        log.info("IP[{}]没有使用记录，可用。",ip);
                         notUseIp = ip;
                         break;
                     }
                 }else {
-                    log.info("IP[{}]没有使用记录，可用。",ip);
+                    log.info("use的大小[0]，说明没有使用记录，可用。");
                     notUseIp = ip;
                     break;
                 }
-            }else {
-                log.info("use的大小[0]，说明没有使用记录，可用。");
-                notUseIp = ip;
-                break;
             }
-        }
 
-        log.info("遍历 IP使用记录  找手机[{}]未使用过的IP，结果：[{}]",imei,notUseIp);
+            log.info("遍历 IP使用记录  找手机[{}]未使用过的IP，结果：[{}]",imei,notUseIp);
 
-        //如果没有找到，说明内存中所有的IP都已经使用过了，发送更换IP请求
-        if(null == notUseIp){
-            log.warn("没有找到，说明内存中所有的IP，手机[{}]都已经使用过了，发送更换IP请求",imei);
-            ipUtil.changIp(false,imei, null);
-            flagGetIp = false;
-            return null;
-        }
-
-        //遍历所有的VPS，找到 该IP对应的VPS
-        String notUseVps = null;
-        for(Map.Entry<String,String> map : Constant.vps.entrySet()){
-            if(notUseIp.equalsIgnoreCase(map.getValue())){
-                notUseVps = map.getKey();
-                break;
+            //如果没有找到，说明内存中所有的IP都已经使用过了，发送更换IP请求
+            if(null == notUseIp){
+                log.warn("没有找到，说明内存中所有的IP，手机[{}]都已经使用过了，发送更换IP请求",imei);
+                ipUtil.changIp(false,imei, null);
+                return null;
             }
-        }
-        log.info("遍历所有的VPS，找到 该IP[{}]对应的VPS，结果：[{}]",notUseIp,notUseVps);
-        if(null != notUseVps){
-            IpPool ipPool = Constant.vps_detail.get(notUseVps);
-            //插入一条使用记录
-            int i = this.insertUseRecord(ipPool.getId(), ipPool.getIp(), imei);
-            if(i > 0){
-                log.info("插入一条使用记录，结果：[{}]",i);
 
-                Map<String,Date> map = new HashMap<>(3);
-                map.put(imei,new Date());
+            //遍历所有的VPS，找到 该IP对应的VPS
+            String notUseVps = null;
+            for(Map.Entry<String,String> map : Constant.vps.entrySet()){
+                if(notUseIp.equalsIgnoreCase(map.getValue())){
+                    notUseVps = map.getKey();
+                    break;
+                }
+            }
+            log.info("遍历所有的VPS，找到 该IP[{}]对应的VPS，结果：[{}]",notUseIp,notUseVps);
+            if(null != notUseVps){
+                IpPool ipPool = Constant.vps_detail.get(notUseVps);
+                //插入一条使用记录
+                int i = this.insertUseRecord(ipPool.getId(), ipPool.getIp(), imei);
+                if(i > 0){
+                    log.info("插入一条使用记录，结果：[{}]",i);
 
-                if(Constant.use.size() > 0){
-                    //有使用记录
-                    Map<String, Date> map1 = Constant.use.get(notUseIp);
-                    if(null != map1){
-                        //把当前手机加入到使用列表
-                        map1.put(imei,new Date());
-                    }else {
+                    Map<String,Date> map = new HashMap<>(3);
+                    map.put(imei,new Date());
+
+                    if(Constant.use.size() > 0){
+                        //有使用记录
+                        Map<String, Date> map1 = Constant.use.get(notUseIp);
+                        if(null != map1){
+                            //把当前手机加入到使用列表
+                            map1.put(imei,new Date());
+                        }else {
+                            Constant.use.put(notUseIp,map);
+                        }
+                    }else{
                         Constant.use.put(notUseIp,map);
                     }
-                }else{
-                    Constant.use.put(notUseIp,map);
+                    return ipPool;
                 }
-                flagGetIp = false;
-                return ipPool;
             }
+        }catch (Exception e){
+            log.error("手机[{}]获取IP失败，原因：[{}]",imei,e.getMessage());
+        }finally {
             flagGetIp = false;
-            return null;
         }
-        flagGetIp = false;
         return null;
     }
 
